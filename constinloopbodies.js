560a9851b4aff49b8bad62f8ae171cb22e6b1780
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-09-04 17:52:44 -0500

    Create constinloopbodies.js

---
+++ constinloopbodies.js
@@ -0,0 +1,52 @@
+//ES6:
+function logArgs(...args){
+  for ( const [index, elem] of args.entries()){
+    const message = index + '.' + elem;
+    console.log(message);
+  }
+}
+logArgs('Hello', 'everyone');
+
+// Output:
+"0.Hello"
+"1.everyone"
+
+//Babel - ES5:
+'use strict';
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+function logArgs() {
+  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = args.entries()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var _step$value = _slicedToArray(_step.value, 2),
+          index = _step$value[0],
+          elem = _step$value[1];
+
+      var message = index + '.' + elem;
+      console.log(message);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+}
+logArgs('Hello', 'everyone');

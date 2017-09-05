bd3984feea970a7c9e35101ff92363742c5c2812
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-09-04 21:33:00 -0500

    Create method.js

---
+++ method.js
@@ -0,0 +1,21 @@
+// ES6: Method definitions
+// Appear inside object literals:
+const obj={
+  add(x,y){
+    return x+y;
+  },
+  sub(x,y){
+    return x-y;
+  }
+};
+
+// Appear inside class definitions:
+class AddSub{
+  add(x,y){
+    return x+y;
+  }
+  sub(x,y){
+    return x-y;
+  }
+};
+
